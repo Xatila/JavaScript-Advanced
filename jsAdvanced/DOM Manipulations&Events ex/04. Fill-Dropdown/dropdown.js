7990b274bf11c93c5fa4907aa33b2b24d21acc77
Kevin Baliev <surprice222@gmail.com>
2022-11-07 18:35:00 +0200

    solving

--- jsAdvanced/DOM Manipulations&Events ex/04. Fill-Dropdown/dropdown.js
+++ jsAdvanced/DOM Manipulations&Events ex/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,18 @@
 function addItem() {
-    console.log('TODO:...');
+    let menu = document.getElementById('menu');
+    let inputText = document.getElementById('newItemText');
+    let inputValue = document.getElementById('newItemValue');
+    let input = [inputText.value, inputValue.value];
+    let option = document.createElement('option');
+    option.textContent=input[0];
+    option.value=input[1];
+    menu.appendChild(option);
+    inputText.value='';
+    inputValue.value='';
+     menu.addEventListener('change', function(element){
+        let selectedText = menu.options[menu.selectedIndex].text;
+        let setValue = element.target.value;
+        inputText.value=selectedText;
+        inputValue.value=setValue;
+    })
 }
--- jsAdvanced/DOM Manipulations&Events ex/05. Encode-and-Decode-Messages/app.js
+++ jsAdvanced/DOM Manipulations&Events ex/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,30 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+  const encodeButton = document.getElementsByTagName("button")[0];
+  const decodeButton = document.getElementsByTagName("button")[1];
+
+  let mainDiv = document.getElementById("main");
+  let areas = mainDiv.getElementsByTagName("textarea");
+  [firstArea, secondArea] = areas;
+  
+  const encode = () => {
+    let result = "";
+    let arrToEncode = Array.from(firstArea.value).toString();
+    for (let i = 0; i < arrToEncode.length; i += 2) {
+      let currentChar = arrToEncode.charCodeAt(i);
+      result += String.fromCharCode(currentChar + 1);
+    }
+    secondArea.textContent = result;
+    decodeButton.addEventListener("click", decode);
+  };
+  const decode = () => {
+    let arrToDecode = Array.from(secondArea.textContent).toString();
+    let result = "";
+    for (let i = 0; i < arrToDecode.length; i += 2) {
+      let currentChar = arrToDecode.charCodeAt(i);
+      result += String.fromCharCode(currentChar - 1);
+    }
+    secondArea.textContent = result;
+    decodeButton.removeEventListener('click', decode);
+  };
+  encodeButton.addEventListener("click", encode);
 }
